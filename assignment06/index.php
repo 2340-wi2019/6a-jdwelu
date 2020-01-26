15ab51a066df51d0433c937097ab73a0b9c7572c
jdwelu <58574621+jdwelu@users.noreply.github.com>
2020-01-26 13:46:24 -0600

    Add files via upload

---
+++ assignment06/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title> Assignment 6 </title>
+    <meta charset="utf-8">
+    <!--
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="js/myScript.js"></script>
+    -->
+</head>
+
+<body>
+    <!--
+        James Welu
+        assignment02.html
+        INFO2340.WW
+        Thoendel
+        01/20/2020
+    -->
+
+<h1>Hello, world!</h1>
+<div>Allow me to introduce myself! My name is <?php echo "James Welu"; ?>!</div>
+<div>I am <?php echo "35"; ?> years old and I am a student and Metro Community College.</div>
+<div>I am currently studying <?php echo "Front End Web Development"; ?> and I hope to graduate in <?php echo "2020"; ?>.</div>
+
+
+</body>
+
+</html>
